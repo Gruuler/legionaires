398a0e279c7cb1f3ee4e3f0cf2d270f597bfc8c9
Chris Nielson <nielson.cj@gmail.com>
2015-05-07 16:55:13 -0600

    login.php
    Changes to login.php.

--- login.php
+++ login.php
@@ -1,9 +1,38 @@
 <?php
 
-	if(isset($_POST['username']) && isset($_POST['password'])) {
+	include 'connect.php';
 
-	}else{
-		header ('location:login.html');
+	if(isset($_POST['uName']) && isset($_POST['password'])) {
+
+
+		$username = $_POST['uName'];
+		$password = sha1($_POST['password']);
+
+		$sql = "SELECT user_id FROM user_account WHERE username = '$username' AND password = '$password'";
+		$results = mysqli_query($link, $sql);
+		echo (!$results?die(mysqli_error($link)."<br>".$sql):"");
+		
+		$count = mysqli_num_rows($results);
+		
+		echo $count;
+
+		if($count > 0){
+			list($userId) = mysqli_fetch_array($results);
+			$time = time();
+			$secret = "what's in a secret?";
+
+			$hash = sha1($userId.$username.$time.$secret);
+
+			$expirationTime = strtotime("+2 years");
+
+			setcookie("user",$userName, $expirationTime);
+			setcookie("userId",$userId, $expirationTime);
+			setcookie("time", $time, $expirationTime);
+			setcookie("hash", $hash, $expirationTime);
+
+			header ('location:index.php');
+		
+		}
 	}
-
+	header ('location:login.html?auth=false');
 ?>
